045daa390a31a4631e60f0b7ad9c601f86c5c840
technicalsupport3 <akashmahto1169@gmail.com>
2025-03-31 18:22:44 +0530

    created a hover item on single pin

--- client/src/components/galleryItem/GalleryItem.jsx
+++ client/src/components/galleryItem/GalleryItem.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./galleryItem.css"
+import { Link } from "react-router"
 
-const GalleryItem = ({item}) => {
+const GalleryItem = ({ item }) => {
   return (
-    <div className='galleryItem' style={{gridRowEnd: `span ${Math.ceil(item.height/100)}`}}>
-        <img src={item.media} alt="" />
-    </div>
+    <div className='galleryItem' style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}>
+      <img src={item.media} alt="" />
+      <Link to={`/pin/${item.id}`} className="overlay"></Link>
+      <button className='saveButton'>Save</button>
+      <div className='overlayIcons'>
+        <button>
+          <img src="/general/share.svg" alt="" />
+        </button>
+        <button>
+          <img src="/general/share.svg" alt="" />
+        </button>
+      </div>
+    </div >
   )
 }
 
