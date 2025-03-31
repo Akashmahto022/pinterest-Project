7fde9c024555e6f758f0a4ddfbe4d75d55e9feb9
technicalsupport3 <akashmahto1169@gmail.com>
2025-03-31 11:35:26 +0530

    setup react router and create a route for page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import HomePage from "./routes/homePage/HomePage.jsx"
+import CreatePage from "./routes/createPage/CreatePage.jsx"
+import PostPage from "./routes/postPage/PostPage.jsx"
+import AuthPage from "./routes/authPage/AuthPage.jsx"
+import { BrowserRouter, Route, Router, Routes } from "react-router"
+import ProfilePage from './routes/profilePage/ProfilePage.jsx'
+import SearchPage from './routes/searchPage/SearchPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/pin/:id' element={<PostPage />} />
+        <Route path='/auth' element={<AuthPage />} />
+        <Route path='/:username' element={<ProfilePage />} />
+        <Route path='/search' element={<SearchPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ client/src/routes/searchPage/SearchPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./searchPage.css"
+
+const SearchPage = () => {
+  return (
+    <div>SearchPage</div>
+  )
+}
+
+export default SearchPage
